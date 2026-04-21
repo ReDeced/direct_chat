52f87567f369d940984b0853d4ae4d49e72ba8aa
ReDeced <romanzorin322@gmail.com>
2026-04-21 18:59:14 +0300

    Добавлено создание сесси для использования api

--- server/server.py
+++ server/server.py
@@ -1,3 +1,4 @@
+import secrets
 import models
 import hashlib
 from argon2 import PasswordHasher
@@ -32,7 +33,10 @@ def register():
     username = data.get("username")
     password = data.get("password")
     public_key = data.get("public_key")
-    
+
+    if not username or not password or not public_key:
+        return jsonify({"status": "error", "error": "Invalid input"})
+   
     password_hash = ph.hash(password)
 
     with Session(engine) as session:
@@ -47,4 +51,42 @@ def register():
         else:
             return jsonify({"status": "error", "error": "User with this username already exists"})
 
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    data = request.json
+    if not data:
+        return jsonify({"status": "error", "error": "No JSON"})
+
+    username = data.get("username")
+    password = data.get("password")
+    
+    if not username or not password:
+        return jsonify({"status": "error", "error": "Invalid input"})
+    
+    with Session(engine) as session:
+        user = session.query(models.User).filter_by(username=username).first()
+
+        if not user:
+            return jsonify({"status": "error", "error": "Invalid credentials"})
+
+        try:
+            ph.verify(user.password_hash, password)
+        except:
+            return jsonify({"status": "error", "error": "Invalid credentials"})
+
+        token = secrets.token_hex(32)
+        
+        session = models.SessionModel(token=token, user_id=user.id)
+
+        return jsonify({
+            "status": "ok",
+            "token": token
+        })
+
+
+def get_user_from_token(token):
+    with Session(engine) as session:
+        s = session.query(models.SessionModel).filter_by(token=token).first()
+        return s.user if s else None
 
